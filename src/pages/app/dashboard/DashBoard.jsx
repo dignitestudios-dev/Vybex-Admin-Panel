e53c27688a23f5371061dd0be856a6ea7aea6199
Austin-seth <183656254+Austin-seth@users.noreply.github.com>
2025-09-19 07:11:35 -0700

    UI complete

--- src/pages/app/dashboard/DashBoard.jsx
+++ src/pages/app/dashboard/DashBoard.jsx
@@ -1,7 +1,8 @@
     import React from 'react'
 import State from '../../../components/app/dashboard/State';
 import { LineGraph } from '../../../components/app/dashboard/LineGraph';
-import HorizontalBarChart from '../../../components/app/dashboard/Barchart';
+import HorizontalBarChart from '../../../components/app/dashboard/HorizontalBarChart';
+
     export default function DashBoard() {
         return (
             <div className='space-y-5'>
